db1a5487e8e438a7c1da1b5499bc5b62fd9fd78f
IloveMonday <iloveyouchandlerbing@gmail.com>
2024-02-01 23:05:30 -0500

    Grid populates, button works

--- script.js
+++ script.js
@@ -1,15 +1,86 @@
-let grid = document.querySelector(".grid");
+      let grid = document.getElementById('grid');
+      let butt = document.querySelector("button");
+      let item = document.querySelectorAll(".item");
 
-let gridNumber = 16;
+      let gridNum = 16;
 
-for (let i = 0; i < gridNumber; i++){
-    let newRow = document.createElement("div");
-    newRow.className = "line";
-    grid.appendChild("newRow");
+      function buildGrid(gridNum) {
+        let divWidth = grid.offsetWidth;
+        let itemSize = (((divWidth/gridNum)/divWidth)*100) + "%";
+        grid.setAttribute("style", `height:${divWidth}px`);
 
-    for (let j = 0; j < gridNumber; j++){
-        let newItem = document.createElement("div");
-        newItem.className = "item";
-        newRow.appendChild(newItem);
-    }
-}
+        boxTotal = gridNum * gridNum;
+        for (let i = 0; i < boxTotal; i++) {
+          let newBox = document.createElement("div");
+          newBox.className = "item";
+          newBox.style.height = itemSize;
+          newBox.style.width = itemSize;
+          grid.appendChild(newBox);
+        }
+      }
+
+      buildGrid(gridNum);
+
+      function getUserNum() {
+        grid.innerHTML = "";
+        let userNum = prompt("How many blocks we doin?", 16);
+        if (userNum > 100 || userNum < 2) {
+          getUserNum();
+        }
+        buildGrid(userNum);
+      }
+
+      butt.addEventListener("click", () => {
+        getUserNum();
+      });
+
+      // newDiv.onMouseOver = (e) => changeColor(e);
+      // function changeColor(event){
+      // parseInt(event.target.style.opacity)
+      //}
+
+
+
+
+
+
+// let grid = document.querySelector("#grid");
+// let butt = document.querySelector("button");
+// let item = document.querySelectorAll(".item");
+
+// let gridNum = 16;
+
+// function buildGrid(gridNum) {
+//     boxTotal = gridNum*gridNum;
+//   for (let i = 0; i < gridNum; i++) {
+//     let newBox = document.createElement("div");
+//     newBox.className = "item";
+//     grid.appendChild(newBox);
+//   }
+// }
+
+// function getUserNum() {
+//   grid.innerHTML = "";
+//   let userNum = prompt("How many blocks we doin?", 16);
+//   if (userNum > 100 || userNum < 2) {
+//     getUserNum();
+//   }
+//     buildGrid(userNum);
+// }
+
+// function setHeight() {
+//   let divWidth = grid.offsetWidth;
+//   grid.setAttribute("style", `height:${divWidth}px`);
+// }
+
+// setHeight();
+// buildGrid(gridNum);
+
+// butt.addEventListener("click", () => {
+//   getUserNum();
+// });
+
+// newDiv.onMouseOver = (e) => changeColor(e);
+// function changeColor(event){
+// event.target.style.opacity
+//}
