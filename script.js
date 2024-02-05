a99fde38cc5fc4535d9530eb0d48f90716b77bfc
IloveMonday <iloveyouchandlerbing@gmail.com>
2024-02-04 22:00:10 -0500

    Draw Function Works

--- script.js
+++ script.js
@@ -7,7 +7,6 @@
       function buildGrid(gridNum) {
         let divWidth = grid.offsetWidth;
         let itemSize = (((divWidth/gridNum)/divWidth)*100) + "%";
-        grid.setAttribute("style", `height:${divWidth}px`);
 
         boxTotal = gridNum * gridNum;
         for (let i = 0; i < boxTotal; i++) {
@@ -34,53 +33,25 @@
         getUserNum();
       });
 
-      // newDiv.onMouseOver = (e) => changeColor(e);
+
+      grid.onmouseover = (e) => {
+         (e.target.style.backgroundColor = "black");
+      //    if (parseInt(e.target.style.opacity) === 0){
+      //     e.target.style.opacity = "0.1";
+      //    }
+      //    else {
+      //     e.target.style.opacity = "0.5";
+      //    }
+
+      //    (e.target.style.opacity = 0.1);
+
+
+
+      //    console.log(e.target.style.opacity);
+
+      }
+      // newBox.onMouseOver = (e) => changeColor(e);
       // function changeColor(event){
+      //   event.target.style.background-color; "red";
       // parseInt(event.target.style.opacity)
-      //}
-
-
-
-
-
-
-// let grid = document.querySelector("#grid");
-// let butt = document.querySelector("button");
-// let item = document.querySelectorAll(".item");
-
-// let gridNum = 16;
-
-// function buildGrid(gridNum) {
-//     boxTotal = gridNum*gridNum;
-//   for (let i = 0; i < gridNum; i++) {
-//     let newBox = document.createElement("div");
-//     newBox.className = "item";
-//     grid.appendChild(newBox);
-//   }
-// }
-
-// function getUserNum() {
-//   grid.innerHTML = "";
-//   let userNum = prompt("How many blocks we doin?", 16);
-//   if (userNum > 100 || userNum < 2) {
-//     getUserNum();
-//   }
-//     buildGrid(userNum);
-// }
-
-// function setHeight() {
-//   let divWidth = grid.offsetWidth;
-//   grid.setAttribute("style", `height:${divWidth}px`);
-// }
-
-// setHeight();
-// buildGrid(gridNum);
-
-// butt.addEventListener("click", () => {
-//   getUserNum();
-// });
-
-// newDiv.onMouseOver = (e) => changeColor(e);
-// function changeColor(event){
-// event.target.style.opacity
-//}
+      // } "rgb(90, 201, 90)")
